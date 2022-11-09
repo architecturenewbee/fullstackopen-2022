8ad10fec3c38aad590102ab942598b074b00c3e6
Gaurav Joshi <gaurav11399joshi@gmail.com>
2022-11-09 22:20:24 +0530

    completed 1.11 & added ancedotes

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const Heading = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <h1>give feedback</h1>
+    </>
+  );
+};
+
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.onClick}>{props.text}</button>
+    </>
+  );
+};
+
+const StatisticLine = (props) => {
+  return (
+    <>
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </tr>
+    </>
+  );
+};
+const Statistic = (props) => {
+  function average() {
+    let avg = (props.good - props.bad) / props.total;
+    return avg;
+  }
+  function positivity() {
+    return (props.good / props.total) * 100 + "%";
+  }
+  if (props.total === 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+  return (
+    <>
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={props.total} />
+          <StatisticLine text="average" value={average()} />
+          <StatisticLine text="positive" value={positivity()} />
+        </tbody>
+      </table>
+    </>
+  );
+};
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  return (
+    <div>
+      <Heading />
+      <Button text={"good"} onClick={() => setGood(good + 1)} />
+      <Button text={"neutral"} onClick={() => setNeutral(neutral + 1)} />
+      <Button text={"bad"} onClick={() => setBad(bad + 1)} />
+      <Statistic
+        good={good}
+        bad={bad}
+        neutral={neutral}
+        total={good + bad + neutral}
+      />
     </div>
   );
-}
+};
 
 export default App;
